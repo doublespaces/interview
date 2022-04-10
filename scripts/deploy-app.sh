fb2ea928b69aeb598e82117b5bbdb269e6ed8a81
doublespaces <doublespaces@gmail.com>
2022-04-09 21:49:05 -0700

    added ssl cert

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,5 +1,7 @@
+sed -i "s/TLS_CRT/${{ secrets.TLS_CRT }}/" kubernetes-ingress/tyler-dev-secret.yaml
+sed -i "s/TLS_KEY/${{ secrets.TLS_KEY }}/" kubernetes-ingress/tyler-dev-secret.yaml
 kubectl apply -f kubernetes-ingress/common/ns-and-sa.yaml
-kubectl apply -f kubernetes-ingress/common/default-server-secret.yaml
+kubectl apply -f kubernetes-ingress/common/tyler-dev-secret.yaml
 kubectl apply -f kubernetes-ingress/common/nginx-config.yaml
 kubectl apply -f kubernetes-ingress/rbac/rbac.yaml
 kubectl apply -f kubernetes-ingress/common/ingress-class.yaml
